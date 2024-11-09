4f719030428d4ff6de388b4ba87dce87c06cbe17
auto-Dog <2969860272@qq.com>
2024-11-09 16:08:36 +0800

    Debug Dataset settings

--- train.py
+++ train.py
@@ -61,8 +61,8 @@ train_val_percent = 0.8
 
 # trainset = CVDcifar('./',train=True,download=True,patch_size=args.patch)
 # testset = CVDcifar('./',train=False,download=True,patch_size=args.patch)
-trainset = CVDImageNet('/kaggle/input/imagenet1k-subset-100k-train-and-10k-val',split='train',patch_size=args.patch)
-valset = CVDImageNet('/kaggle/input/imagenet1k-subset-100k-train-and-10k-val',split='val',patch_size=args.patch)
+trainset = CVDImageNet('/kaggle/input/imagenet1k-subset-100k-train-and-10k-val',split='imagenet_subtrain',patch_size=args.patch)
+valset = CVDImageNet('/kaggle/input/imagenet1k-subset-100k-train-and-10k-val',split='imagenet_subval',patch_size=args.patch)
 inferenceset = CIFAR10('./',train=False,download=True,transform=transforms.Compose([transforms.ToTensor(),]))
 
 # train_size = int(len(trainset) * train_val_percent)   # not suitable for ImageNet subset
